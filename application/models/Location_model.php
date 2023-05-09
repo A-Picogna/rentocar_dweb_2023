73b4fd22f4bc9d3e59db46fe743d0e711341bc40
sablegall <sabrinalegall7@gmail.com>
2023-05-09 14:10:22 +0200

    test

--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -3,6 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Location_model extends CI_Model {
 
+    function __construct(){
+        $this->load->database();
+    }
 
+    public function get_location()	{
+
+	}
+
+	public function delete_location()	{
+        $this->db->query('DELETE FROM location WHERE id = $id_loc');
+
+	}
+
+	public function add_location()	{
+
+	}
 
 }
