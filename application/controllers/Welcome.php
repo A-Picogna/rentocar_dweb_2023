8c87beba5f1ad1d386b27873023906dea7effc69
apicogna <alex.picogna@wanadoo.fr>
2023-05-09 14:55:44 +0200

    add js directory add css directory create style.css add some tests css in welcome controller and view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,11 +19,7 @@ class Welcome extends CI_Controller {
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
 	public function index()	{
-		$this->load->view('welcome_message');
-	}
-
-
-	public function tata($param1 = "")	{
+		$this->load->helper('url');
 		$this->load->view('welcome_message');
 	}
 }
