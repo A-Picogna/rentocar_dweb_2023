aaeb2eb08626150f0a8494d1709e846f3383cde5
apicogna <alex.picogna@wanadoo.fr>
2023-05-09 11:09:02 +0200

    create models client, location, vehicule

---
+++ application/models/Vehicule_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vehicule_model extends CI_Model {
+
+	public function get_vehicule()	{
+
+	}
+
+	public function delete_vehicule()	{
+
+	}
+
+	public function add_vehicule()	{
+
+	}
+
+}
