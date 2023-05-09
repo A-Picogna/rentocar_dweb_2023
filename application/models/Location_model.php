df2f092e6311cac039599150db6f493088267c60
sablegall <sabrinalegall7@gmail.com>
2023-05-09 16:19:59 +0200

    locations

--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -3,15 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Location_model extends CI_Model {
 
-    function __construct(){
+    public function __construct(){
         $this->load->database();
     }
 
-    public function get_location()	{
+    public function get_location($id)	{
+		$locations = $this->db->query("SELECT * FROM location");
 
+    	foreach($locations as $location){ 
+        ?>
+            <h4> <?= $location['date_debut'] ?> </h4>
+            <h4> <?= $location['date_fin'] ?> </h4>
+            <h4> <?= $location['nb_km'] ?> </h4>
+            <h4> <?= $location['prix'] ?> </h4>
+            <h4> <?= $location['utilisateur_id'] ?> </h4>
+            <h4> <?= $location['vehicule_id'] ?> </h4>
+    <?php } 
 	}
 
 	public function delete_location()	{
+        $id_loc = $this->input->post('id');
         $this->db->query('DELETE FROM location WHERE id = $id_loc');
 
 	}
