560493ba46d5c766cd108c01e701178c2efe3ad7
lolo298 <lorenzoaoulini@gmail.com>
2023-05-09 14:09:17 +0200

    Update users error messages

--- application/views/edit_profil.php
+++ application/views/edit_profil.php
@@ -18,28 +18,36 @@
       justify-content: center;
       height: 100vh;
     }
+
+    .error {
+      color: red;
+      font-size: 12px;
+    }
   </style>
 </head>
 
 <body>
-  <?php echo validation_errors(); ?>
   <?php echo form_open('profil/edit') ?>
   <label>
     Nom:
     <input type="text" name="nom" value="<?= $client['nom'] ?>">
   </label>
+  <?= form_error('nom', '<div class="error">', '</div>'); ?>
   <label>
     Prenom:
     <input type="text" name="prenom" value="<?= $client['prenom'] ?>">
   </label>
+  <?= form_error('prenom', '<div class="error">', '</div>'); ?>
   <label>
     Date de naissance:
     <input type="date" name="ddn" value="<?= $client['ddn'] ?>">
   </label>
+  <?= form_error('date', '<div class="error">', '</div>'); ?>
   <label>
     Email:
     <input type="email" name="email" value="<?= $client['email'] ?>">
   </label>
+  <?= form_error('email', '<div class="error">', '</div>'); ?>
 
   <a href="<?php echo site_url('profil') ?>">Cancel</a>
   <input type="submit" value="Save">
