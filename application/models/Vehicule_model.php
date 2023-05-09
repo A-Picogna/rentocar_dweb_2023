e42042c2ce2db5c1dfcb9fd4c12bb69d1eff8707
tanguyleboss <71895709+tanguyleboss@users.noreply.github.com>
2023-05-09 14:06:52 +0200

    Update Vehicule_model.php

--- application/models/Vehicule_model.php
+++ application/models/Vehicule_model.php
@@ -5,6 +5,23 @@ class Vehicule_model extends CI_Model {
 
 	public function get_vehicule()	{
 
+		$this->load->database();
+
+
+		
+		$vehicules = $this->db->query("SELECT * FROM vehicule");
+
+    	foreach($vehicules as $vehicule): ?>
+
+			<h4> <?= $vehicule['type_vehicule'] ?> </h4>
+			<h4> <?= $vehicule['modele'] ?> </h4>
+			<h4> <?= $vehicule['marque'] ?> </h4>
+			<h4> <?= $vehicule['prix_location'] ?> </h4>
+
+			<h4 type="submit" name="submit">Voir le véhicule</h4>
+        
+    <?php endforeach;  
+
 	}
 
 	public function delete_vehicule()	{
