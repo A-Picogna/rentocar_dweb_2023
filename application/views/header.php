43e8c1db75bf60324d6429b9e753fb53879e173b
Laura <106605150+LauraR40@users.noreply.github.com>
2023-05-23 15:27:28 +0200

    ajout au header

--- application/views/header.php
+++ application/views/header.php
@@ -1,20 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
+// $autoload['helper'] = array('url');
 ?>
 <!DOCTYPE html>
 <html lang="" class="">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+<<<<<<< Updated upstream
     <link href="<?=base_url() ?>/css/nav.css" rel="stylesheet">
     <link href="<?=base_url() ?>/css/loc.css" rel="stylesheet">
     <link href="<?=base_url() ?>/css/assets.css" rel="stylesheet">
     <link href="<?=base_url() ?>/css/style.css" rel="stylesheet">
 
+=======
+    <link href="<?= base_url() ?>css/nav.css" rel="stylesheet">
+>>>>>>> Stashed changes
 </head>
 
 <body>
+<<<<<<< Updated upstream
 <nav class="main-navbar">
 		<a class="" href="../index.php">
 			<img class="img-64px logo" src="" alt="logo" />
@@ -27,3 +34,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</ul>
 		</div>
 </nav>
+=======
+    <nav class="main-navbar">
+        <div>
+            <a class="" href="#">
+                <img class="img-64px logo" src="" alt="logo" />
+            </a>
+            <div class="navbar-body" id="navbarNavDropdown">
+                <ul>
+                    <li><a href="<?= site_url() ?>/Location">Louer</a></li>
+                    <li><a href="<?= site_url() ?>/Profil">Profil</a></li>
+                    <li><a href="<?= site_url() ?>/Vehicule">Louer</a></li>
+                </ul>
+                <hr class="menu-sep">
+                <a class="contact-button" href="#">Contact</a>
+            </div>
+        </div>
+    </nav>
+>>>>>>> Stashed changes
