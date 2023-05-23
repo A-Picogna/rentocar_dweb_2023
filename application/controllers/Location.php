c39ba04194bf1dc9bd2dbdc0a79a984552fe945b
LaPoloche <buchon.paul@gmail.com>
2023-05-23 14:46:29 +0200

    Page de location stylée et fonctionnelle

--- application/controllers/Location.php
+++ application/controllers/Location.php
@@ -14,14 +14,16 @@ class Location extends CI_Controller {
 	public function index()	{
 		$this->load->helper(array('form', 'url'));
 		$data['vehicule_info'] = $this->get_vehicule_info(1);
-		$data['form_open'] = form_open('Location/form_validation');
+		$data['form_open'] = form_open('Location/form_validation','class="form-example"');
 		$this->load->vars($data);
+		$this->load->view('header');
 		$this->load->view('location_view');
+		$this->load->view('footer');
 	}
 
 	public function form_validation(){
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('deb', '', 'required');
+		$this->form_validation->set_rules('deb', '', 'required|callback_date_check');
 		$this->form_validation->set_rules('fin', '', 'required');
 		$this->form_validation->set_rules('km', '', 'required|numeric');
 		
@@ -34,7 +36,18 @@ class Location extends CI_Controller {
 			$this->Location_model->add_location($this->input->post());
 		}
 	}
-    
+	public function date_check()
+	{
+			if (count($this->Location_model->verif_dates($this->input->post('deb'),$this->input->post('fin'))) < 1)
+			{
+					$this->form_validation->set_message('date_check', 'La date de début doit être supérieur à la date du jour et inférieur à la date de fin');
+					return FALSE;
+			}
+			else
+			{
+					return TRUE;
+			}
+	}
     public function delete($id){
         $del = $this->Location_model->delete_location($id);
         if($del == false){
