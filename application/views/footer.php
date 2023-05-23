7c7f102dec03cec53225d0122d7341d1dd837d5c
Alexandre Picogna <alex.picogna@wanadoo.fr>
2023-05-23 10:20:06 +0200

    add proto home page and nav bar (no css)

---
+++ application/views/footer.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<footer class="footer">
+	<nav class="bottom-navbar">
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Mentions légales</a></li>
+		</ul>
+	</nav>
+</footer>
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="" class="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="<?=base_url() ?>/css/nav.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="main-navbar">
+	<div>
+		<a class="" href="#">
+			<img class="img-64px logo" src="" alt="logo" />
+		</a>
+		<div class="navbar-body" id="navbarNavDropdown">
+			<ul>
+				<li><a href="<?=site_url() ?>/Location">Louer</a></li>
+				<li><a href="<?=site_url() ?>/Profil">Profil</a></li>
+				<li><a href="<?=site_url() ?>/Vehicule">Louer</a></li>
+			</ul>
+			<hr class="menu-sep">
+			<a class="contact-button" href="#">Contact</a>
+		</div>
+	</div>
+</nav>
