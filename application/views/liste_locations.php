2567d4b4c35c2ed9f3b0f6319034694a737a9953
sablegall <sabrinalegall7@gmail.com>
2023-05-23 15:22:46 +0200

    Update affichage liste locations

--- application/views/liste_locations.php
+++ application/views/liste_locations.php
@@ -1,11 +1,12 @@
-<h2>Vos locations</h2>
+<h1>Vos locations</h1>
 
+<h2>En cours</h2>
+
+<h2>A venir</h2>
 <?php foreach ($locations as $loc){?>
     <img src="" alt="">
     <h4> <?=$loc['date_debut']?> </h4>
     <h4> <?=$loc['vehicule_id']?> </h4>
 
     <a href="<?= site_url();?>/location/delete/<?=$loc['id']?>"><button>Annuler</button></a>
-
-
 <?php };?>
