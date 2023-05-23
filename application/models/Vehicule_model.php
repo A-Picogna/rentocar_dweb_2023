4da3c8000bd1728179c927e8ba5835a9c66f886c
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2023-05-23 14:06:02 +0200

    ajout vehicule

--- application/models/Vehicule_model.php
+++ application/models/Vehicule_model.php
@@ -14,58 +14,55 @@ class Vehicule_model extends CI_Model
 
         $vehicules = $this->db->query("SELECT * FROM vehicule");
         $vehicules = $vehicules->result_array();
-		return $vehicules;
+        return $vehicules;
     }
 
     public function delete_vehicule($id)
     {
-			$res = false;
+        $res = false;
 
-			if(!$this->db->query("SELECT id FROM vehicule WHERE id = $id")->count() == 0) {
-				$res = $this->db->query("DELETE FROM vehicule WHERE id = $id");
-			}
-
-			return $res;
-    }
-
-    public function add_vehicule(){
-        /*
-        if (isset($_POST['submit'])) {
-            $type_vehicule = $_POST['type_vehicule'];
-            $kilometrage = $_POST['kilometrage'];
-            $nb_places = $_POST['nb_places'];
-            $marque = $_POST['marque'];
-            $modele = $_POST['modele'];
-            $puissance = $_POST['puissance'];
-            $prix_location = $_POST['prix_location'];
-            $etat = $_POST['etat'];
-            $vitesse_max = $_POST['vitesse_max'];
-
-            $this->db->query(
-                "INSERT INTO vehicule(type_vehicule,kilometrage,nb_places,marque,modele,puissance,prix_location,etat,vitesse_max) VALUES(:type_vehicule,:kilometrage,:nb_places,:marque,:modele,:puissance,:prix_location,:etat,:vitesse_max)",
-                ["type_vehicule" => $type_vehicule, "kilometrage" => $kilometrage, "nb_places" => $nb_places, "marque" => $marque, "modele" => $modele, "puissance" => $puissance, "prix_location" => $prix_location, "etat" => $etat, "vitesse_max" => $vitesse_max]
-            );
-
+        if (!$this->db->query("SELECT id FROM vehicule WHERE id = $id")->count() == 0) {
+            $res = $this->db->query("DELETE FROM vehicule WHERE id = $id");
         }
-        */
+
+        return $res;
     }
-	public function edit_vehicule($id, $form) {
-		$type_vehicule = $form['type_vehicule'];
-		$kilometrage = $form['kilometrage'];
-		$nb_places = $form['nb_places'];
-		$marque = $form['marque'];
-		$modele = $form['modele'];
-		$puissance = $form['puissance'];
-		$prix_location = $form['prix_location'];
-		$etat = $form['etat'];
-		$vitesse_max = $form['vitesse_max'];
 
-		$this->db->query(
-			"UPDATE vehicule SET type_vehicule = $type_vehicule, kilometrage = $kilometrage, bn_places = $nb_places, marque = $marque, modele = $modele, puissance = $puissance, prix_location = $prix_location, etat = $etat, vitesse_max = $vitesse_max WHERE id=$id"
-		);
-	}
+    public function add_vehicule($type_vehicule, $kilometrage, $nb_places, $marque, $modele, $puissance, $prix_location, $etat, $vitesse_max)
+    {
+        $data = array(
+            'type_vehicule' => $type_vehicule,
+            'kilometrage' => $kilometrage,
+            'nb_places' => $nb_places,
+            'marque' => $marque,
+            'modele' => $modele,
+            'puissance' => $puissance,
+            'prix_location' => $prix_location,
+            'etat' => $etat,
+            'vitesse_max' => $vitesse_max
+        );
+        $this->db->insert('vehicule', $data);
+    }
 
-    public function get_vehicule_by_id($id){
+    public function edit_vehicule($id, $form)
+    {
+        $type_vehicule = $form['type_vehicule'];
+        $kilometrage = $form['kilometrage'];
+        $nb_places = $form['nb_places'];
+        $marque = $form['marque'];
+        $modele = $form['modele'];
+        $puissance = $form['puissance'];
+        $prix_location = $form['prix_location'];
+        $etat = $form['etat'];
+        $vitesse_max = $form['vitesse_max'];
+
+        $this->db->query(
+            "UPDATE vehicule SET type_vehicule = $type_vehicule, kilometrage = $kilometrage, bn_places = $nb_places, marque = $marque, modele = $modele, puissance = $puissance, prix_location = $prix_location, etat = $etat, vitesse_max = $vitesse_max WHERE id=$id"
+        );
+    }
+
+    public function get_vehicule_by_id($id)
+    {
         $query = $this->db->query("SELECT * FROM vehicule WHERE id = $id");
         return $query->result_array()[0];
     }
--- application/views/ajout_vehicule.php
+++ application/views/ajout_vehicule.php
@@ -1,53 +1,52 @@
+<?php
+echo validation_errors();
+echo form_open('vehicule/ajouter') ?>
 
-<?php echo form_open('vehicule/ajouter') ?>
 
-
-  <label>
-    Type de véhicule :
-    <input type="text" name="type_vehicule" >
-  </label>
-  <?= form_error('type_vehicule', '<div class="error">', '</div>'); ?>
-  <label>
+<label>
+  Type de véhicule :
+  <input type="text" name="type_vehicule" value="<?= set_value('type_vehicule') ?>">
+</label>
+<?= form_error('type_vehicule', '<div class="error">', '</div>'); ?>
+<label>
   Kilométrage :
-    <input type="number" name="kilometrage" >
-  </label>
-  <?= form_error('kilometrage', '<div class="error">', '</div>'); ?>
-  <label>
-    Nombre de places :
-    <input type="number" name="nb_places" >
-  </label>
-  <?= form_error('nb_places', '<div class="error">', '</div>'); ?>
-  <label>
-    Marque :
-    <input type="text" name="marque" >
-  </label>
-  <?= form_error('marque', '<div class="error">', '</div>'); ?>
-  <label>
-    Modèle :
-    <input type="text" name="modele" >
-  </label>
-  <?= form_error('modele', '<div class="error">', '</div>'); ?>
-  <label>
-    Puissance :
-    <input type="number" name="puissance" >
-  </label>
-  <?= form_error('puissance', '<div class="error">', '</div>'); ?>
-  <label>
-    Prix de location :
-    <input type="number" name="prix_location" >
-  </label>
-  <?= form_error('prix_location', '<div class="error">', '</div>'); ?>
-  <label>
-    Etat :
-    <input type="text" name="Etat" >
-  </label>
-  <?= form_error('Etat', '<div class="error">', '</div>'); ?>
-  <label>
-    Vitesse max :
-    <input type="number" name="vitesse_max" >
-  </label>
-  <?= form_error('vitesse_max', '<div class="error">', '</div>'); ?>
+  <input type="number" name="kilometrage" value="<?= set_value('kilometrage') ?>">
+</label>
+<?= form_error('kilometrage', '<div class="error">', '</div>'); ?>
+<label>
+  Nombre de places :
+  <input type="number" name="nb_places" value="<?= set_value('nb_places') ?>">
+</label>
+<?= form_error('nb_places', '<div class="error">', '</div>'); ?>
+<label>
+  Marque :
+  <input type="text" name="marque" value="<?= set_value('marque') ?>">
+</label>
+<?= form_error('marque', '<div class="error">', '</div>'); ?>
+<label>
+  Modèle :
+  <input type="text" name="modele" value="<?= set_value('modele') ?>">
+</label>
+<?= form_error('modele', '<div class="error">', '</div>'); ?>
+<label>
+  Puissance :
+  <input type="number" name="puissance" value="<?= set_value('puissance') ?>">
+</label>
+<?= form_error('puissance', '<div class="error">', '</div>'); ?>
+<label>
+  Prix de location :
+  <input type="number" name="prix_location" value="<?= set_value('prix_location') ?>">
+</label>
+<?= form_error('prix_location', '<div class="error">', '</div>'); ?>
+<label>
+  Etat :
+  <input type="text" name="etat" value="<?= set_value('etat') ?>">
+</label>
+<?= form_error('Etat', '<div class="error">', '</div>'); ?>
+<label>
+  Vitesse max :
+  <input type="number" name="vitesse_max" value="<?= set_value('vitesse_max') ?>">
+</label>
+<?= form_error('vitesse_max', '<div class="error">', '</div>'); ?>
 
-  <input type="submit" value="Save">
-    
-
+<button type="submit">Save</button>
