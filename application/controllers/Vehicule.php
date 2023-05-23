3427f2fa47bcc78a1c8c817111b13bc62225c6d0
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2023-05-23 11:15:36 +0200

    ajouter vehicule controller modif

--- application/controllers/Vehicule.php
+++ application/controllers/Vehicule.php
@@ -1,17 +1,19 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Vehicule extends CI_Controller {
+class Vehicule extends CI_Controller
+{
 
 	public function __construct() {
 
 		
 		parent::__construct();
-		$this->load->helper('form');
+		$this->load->helper('form','url');
+		$this->load->library('form_validation');
 		$this->load->model('Vehicule_model');
 	}
 
-	public function index()	{
+	public function index($message = null)	{
 		$data["vehicules"] = $this->Vehicule_model->get_all_vehicule();
 
 
@@ -19,15 +21,38 @@ class Vehicule extends CI_Controller {
 		$this->load->view("liste_vehicules");
 	}
 
-	public function ajouter()	{
-		$this->Vehicule_model->add_vehicule();
-		$this->load->view("ajout_vehicule");
+	public function ajouter()
+	{
+		$this->form_validation->set_rules('type_vehicule', 'Type de véhicule', 'required');
+		$this->form_validation->set_rules('kilometrage', 'Kilométrage', 'required|numeric');
+		$this->form_validation->set_rules('nb_places', 'Nombre de places', 'required|numeric');
+		$this->form_validation->set_rules('marque', 'Marque', 'required');
+		$this->form_validation->set_rules('modele', 'Modèle', 'required');
+		$this->form_validation->set_rules('puissance', 'Puissance', 'required');
+		$this->form_validation->set_rules('prix_location', 'Prix de location', 'required');
+		$this->form_validation->set_rules('etat', 'Etat', 'required');
+		$this->form_validation->set_rules('vitesse_max', 'Vitesse max', 'required');
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view("ajout_vehicule");
+		} else {
+			$type_vehicule = $this->input->post('type_vehicule');
+			$kilometrage = $this->input->post('kilometrage');
+			$nb_places = $this->input->post('nb_places');
+			$marque = $this->input->post('marque');
+			$modele = $this->input->post('modele');
+			$puissance = $this->input->post('puissance');
+			$prix_location = $this->input->post('prix_location');
+			$etat = $this->input->post('etat');
+			$vitesse_max = $this->input->post('vitesse_max');
+
+			$this->Vehicule_model->add_vehicule($type_vehicule, $kilometrage, $nb_places, $marque, $modele, $puissance, $prix_location, $etat, $vitesse_max);
+			$this->index("Véhicule ajouté avec succès");
+		}
 	}
 
-	public function supprimer($id)	{
+	public function supprimer($id)
+	{
 		$this->Vehicule_model->delete_vehicule($id);
 	}
-
 }
-
-?>
--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -1,11 +1,13 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Location_model extends CI_Model {
+class Location_model extends CI_Model
+{
 
-    public function __construct(){
-        $this->load->database();
-    }
+  public function __construct()
+  {
+    $this->load->database();
+  }
 
 	public function delete_location($id)	{
         $q = $this->db->query("SELECT date_debut FROM location WHERE utilisateur_id = $id");
@@ -17,15 +19,24 @@ class Location_model extends CI_Model {
         else {
             return false;
         }
+  }
+  
+  public function add_location()
+  {
+  }
 
-	}
-
-	public function add_location()	{
-
-	}
-
-  public function get_user_location($id) {
+  public function get_user_location($id)
+  {
     $query = $this->db->query("SELECT * FROM location WHERE utilisateur_id = $id");
     return $query->result_array();
+  }
+
+  public function get_location_price(array $post_data)
+  {
+    $datetime1 = new DateTime($post_data['date_debut']);
+    $datetime2 = new DateTime($post_data['date_fin']);
+    $interval = $datetime1->diff($datetime2);
+    $nb_jours = $interval->days;
+    $price = 0;
   }
 }
