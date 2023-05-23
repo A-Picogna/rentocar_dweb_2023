c70c5a61e5bac16c589cbcb0505b9a3cabb390c4
Laura <106605150+LauraR40@users.noreply.github.com>
2023-05-23 15:27:13 +0200

    rectification landing

--- application/views/landing.php
+++ application/views/landing.php
@@ -5,8 +5,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <link rel="stylesheet" href="css/nav.css">
 
 <div id="container">
-    <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
-        aria-hidden="true">
+    <!-- MODEL -->
+    <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -18,15 +18,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 <div class="modal-body">
                     <form action="layouts/change_password.php" method="POST">
                         <label for='current_password'>Mot de passe actuel</label>
-                        <input type="password" id="current_password" name="current_password" class="form-control"
-                            required />
+                        <input type="password" id="current_password" name="current_password" class="form-control" required />
                         <br />
                         <label for='new_password'>Nouveau mot de passe</label>
                         <input type="password" id="new_password" name="new_password" class="form-control" required />
                         <br />
                         <label for='new_password_retype'>Re tapez le nouveau mot de passe</label>
-                        <input type="password" id="new_password_retype" name="new_password_retype" class="form-control"
-                            required />
+                        <input type="password" id="new_password_retype" name="new_password_retype" class="form-control" required />
                         <br />
                         <button type="submit" class="btn btn-success">Sauvegarder</button>
                     </form>
