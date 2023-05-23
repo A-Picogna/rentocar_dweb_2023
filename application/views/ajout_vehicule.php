14b609fbfcdc308bf0e004f168b6bb218bc0616e
tanguyleboss <71895709+tanguyleboss@users.noreply.github.com>
2023-05-23 14:26:06 +0200

    css

--- application/views/ajout_vehicule.php
+++ application/views/ajout_vehicule.php
@@ -3,50 +3,60 @@ echo validation_errors();
 echo form_open('vehicule/ajouter') ?>
 
 
-<label >
+<label class="ajout">
   <p>Type de véhicule :</p>
-  <input type="text" name="type_vehicule" value="<?= set_value('type_vehicule') ?>"class="btn ajout">
+  <select name="type_vehicule" class="btn ajout">
+    <option value="voiture">Voiture</option>
+    <option value="camion">Camion</option>
+    <option value="moto">Moto</option>
+    <option value="utilitaire">Utilitaire</option>
+  </select>
 </label>
 <?= form_error('type_vehicule', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
     <p>Kilométrage :</p>
-  <input type="number" name="kilometrage" value="<?= set_value('kilometrage') ?>"class="btn ajout">
+  <input type="number" name="kilometrage" value="<?= set_value('kilometrage') ?>"class="btn">
 </label>
 <?= form_error('kilometrage', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Nombre de places :</p>
-  <input type="number" name="nb_places" value="<?= set_value('nb_places') ?>"class="btn ajout">
+  <input type="number" name="nb_places" value="<?= set_value('nb_places') ?>"class="btn">
 </label>
 <?= form_error('nb_places', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Marque :</p>
-  <input type="text" name="marque" value="<?= set_value('marque') ?>"class="btn ajout">
+  <input type="text" name="marque" value="<?= set_value('marque') ?>"class="btn">
 </label>
 <?= form_error('marque', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Modèle :</p>
-  <input type="text" name="modele" value="<?= set_value('modele') ?>"class="btn ajout">
+  <input type="text" name="modele" value="<?= set_value('modele') ?>"class="btn">
 </label>
 <?= form_error('modele', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Puissance :</p>
-  <input type="number" name="puissance" value="<?= set_value('puissance') ?>"class="btn ajout">
+  <input type="number" name="puissance" value="<?= set_value('puissance') ?>"class="btn">
 </label>
 <?= form_error('puissance', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Prix de location :</p>
-  <input type="number" name="prix_location" value="<?= set_value('prix_location') ?>"class="btn ajout">
+  <input type="number" name="prix_location" value="<?= set_value('prix_location') ?>"class="btn">
 </label>
 <?= form_error('prix_location', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Etat :</p>
-  <input type="text" name="etat" value="<?= set_value('etat') ?>"class="btn ajout">
+  <select name="type_vehicule" class="btn ajout">
+    <option value="neuf">Neuf</option>
+    <option value="tresBonEtat">Très bon état</option>
+    <option value="bonEtat">Bon état</option>
+    <option value="etatDiscutable">état discutable</option>
+  </select>
 </label>
 <?= form_error('Etat', '<div class="error">', '</div>'); ?>
-<label >
+<label class="ajout">
   <p>Vitesse max :</p>
-  <input type="number" name="vitesse_max" value="<?= set_value('vitesse_max') ?>"class="btn ajout">
+  <input type="number" name="vitesse_max" value="<?= set_value('vitesse_max') ?>"class="btn">
 </label>
-<?= form_error('vitesse_max', '<div class="error">', '</div>'); ?>
 
-<button type="submit">Save</button>
+<input type="submit" value="Save" class="btn">
+    
